d8a53d9833956438e9dab62ea0a614b12c5facc8
vsyang <yang.vanessa85@gmail.com>
2026-03-25 15:42:31 -0600

    stashing to merge main

--- Data/PetStoreContext.cs
+++ Data/PetStoreContext.cs
@@ -1,19 +1,23 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using PetStore.Models;
 
 namespace PetStore.Data;
 
-public class PetStoreContext : DbContext
+public class PetStoreContext : IdentityDbContext<ApplicationUser>
 {
   public PetStoreContext(DbContextOptions<PetStoreContext> options)
       : base(options)
   {
   }
 
-  public DbSet<Pet> Pets => Set<Pet>();
+  public DbSet<Pet> Pets { get; set; }
+
+  public DbSet<PetCategory> PetCategories { get; set; }
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
+    base.OnModelCreating(modelBuilder);
     modelBuilder.Entity<Pet>().HasData(
         new Pet
         {
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PetStore.Models;
+
+public class ApplicationUser : IdentityUser
+{
+  [Required]
+  [MaxLength(50)]
+  [Display(Name = "Name")]
+  public string FirstName { get; set; } = string.Empty;
+
+  [Required]
+  [MaxLength(50)]
+  [Display(Name = "Last Name")]
+  public string LastName { get; set; } = string.Empty;
+
+  [MaxLength(20)]
+  [Display(Name = "Nick name")]
+  public string? NickName { get; set; } = string.Empty;
+
+  [MaxLength(100)]
+  [Display(Name = "Address")]
+  public string? Address { get; set; } = string.Empty;
+
+  [Display(Name = "Date of Birth")]
+  public DateTime? DateOfBirth { get; set; }
+
+  public string DisplayNam => !string.IsNullOrEmpty(NickName) ? NickName : $"{FirstName} {LastName}";
+}
