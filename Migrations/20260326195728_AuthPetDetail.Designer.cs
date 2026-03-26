dcb835fd48b1269821a6ac698ae5a1116fdcc340
vsyang <yang.vanessa85@gmail.com>
2026-03-26 13:58:08 -0600

    migration updated

--- Migrations/20260326195728_AuthPetDetail.Designer.cs
+++ Migrations/20260326195728_AuthPetDetail.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PetStore.Data;
 
@@ -10,9 +11,11 @@ using PetStore.Data;
 namespace PetStore.Migrations
 {
     [DbContext(typeof(PetStoreContext))]
-    partial class PetStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20260326195728_AuthPetDetail")]
+    partial class AuthPetDetail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.5");
