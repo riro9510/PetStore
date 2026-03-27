8b13d84f18fb395beefd24d42ccd12220660fef8
vsyang <yang.vanessa85@gmail.com>
2026-03-26 21:26:29 -0600

    updated validations and css

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,13 +5,13 @@ namespace PetStore.Models;
 public class Pet
 {
   public int Id { get; set; }
-  [Required(ErrorMessage = "Pet name is required")]
+  [Required(ErrorMessage = "*Pet name is required")]
   public string Name { get; set; } = "";
-  [Required(ErrorMessage = "Pet must have a category")]
+  [Required(ErrorMessage = "*Pet must have a category")]
   public string Category { get; set; } = "";
-  [Required(ErrorMessage = "Pet breed is required")]
+  [Required(ErrorMessage = "*Pet breed is required")]
   public string Breed { get; set; } = "";
-  [Range(0,1000, ErrorMessage = "Age must be between 0 and 1000")]
+  [Range(0,1000, ErrorMessage = "*Age must be between 0 and 1000")]
   public int Age { get; set; }
   public string Description { get; set; } = "";
   public string ImageUrl { get; set; } = "";
