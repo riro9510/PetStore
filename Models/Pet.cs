c9a7a25fd78bdae09d5d1203a6f8a1972c244df8
vsyang <yang.vanessa85@gmail.com>
2026-03-26 19:10:51 -0600

    created categories drop down

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -7,8 +7,11 @@ public class Pet
   public int Id { get; set; }
   [Required]
   public string Name { get; set; } = "";
+  [Required]
   public string Category { get; set; } = "";
+  [Required]
   public string Breed { get; set; } = "";
+  [Required]
   public int Age { get; set; }
   public string Description { get; set; } = "";
   public string ImageUrl { get; set; } = "";
