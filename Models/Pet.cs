b7bc37e48301af28b4237f6e0486a2fda1089721
vsyang <yang.vanessa85@gmail.com>
2026-03-26 21:04:47 -0600

    add/edit form validation added

--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,13 +5,13 @@ namespace PetStore.Models;
 public class Pet
 {
   public int Id { get; set; }
-  [Required]
+  [Required(ErrorMessage = "Pet name is required")]
   public string Name { get; set; } = "";
-  [Required]
+  [Required(ErrorMessage = "Pet must have a category")]
   public string Category { get; set; } = "";
-  [Required]
+  [Required(ErrorMessage = "Pet breed is required")]
   public string Breed { get; set; } = "";
-  [Required]
+  [Range(0,1000, ErrorMessage = "Age must be between 0 and 1000")]
   public int Age { get; set; }
   public string Description { get; set; } = "";
   public string ImageUrl { get; set; } = "";
