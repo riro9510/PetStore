d709ed6184d5a363e23f79f36346314cc0920cff
vsyang <yang.vanessa85@gmail.com>
2026-04-09 00:37:14 -0600

    don't be mad, added shelter name and synced to database

--- Migrations/20260409050610_SyncModelAfterMerge.Designer.cs
+++ Migrations/20260409050610_SyncModelAfterMerge.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PetStore.Data;
 
@@ -11,9 +12,11 @@ using PetStore.Data;
 namespace PetStore.Migrations
 {
     [DbContext(typeof(PetStoreContext))]
-    partial class PetStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20260409050610_SyncModelAfterMerge")]
+    partial class SyncModelAfterMerge
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
